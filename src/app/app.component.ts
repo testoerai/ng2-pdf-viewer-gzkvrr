cc068833590b4fbc014f3cfb2df50d8c03f6b93c
testoerai <119046862+testoerai@users.noreply.github.com>
2022-11-27 23:15:23 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,34 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
+  pdfSource = 'https://www.orimi.com/pdf-test.pdf';
   src: any;
 
   ngOnInit() {
-    this.src = this.test();
-    console.log("test");
+    this.src = this._base64ToArrayBuffer(this.pdfSource);
   }
 
   test() {
-    console.log("test");
-    return this.http.get(
-      `https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf`,
-      { responseType: 'arraybuffer' as 'json' }
-    );
+    this.src = this.http.get(`assets/test.pdf`, {
+      responseType: 'arraybuffer' as 'json',
+    });
+  }
+
+  autreTest() {
+    this.src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
+  }
+
+  dernierTest() {
+    this.src = this._base64ToArrayBuffer(this.pdfSource);
+  }
+
+  _base64ToArrayBuffer(base64) {
+    const binary_string = window.atob(bas64);
+    const len = binary_string.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
   }
 }
